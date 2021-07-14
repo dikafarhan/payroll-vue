9160a34a7d675bfb6d54d4410b3427917553a403
blackhunter lala <blackhunterganzzfct@gmail.com>
2021-07-14 09:40:16 +0700

    vue-payroll

---
+++ src/views/auth/Register.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="register">
+        HALAMAN REGISTER
+        <div class="account">
+  <router-link :to="{name: 'login'}" class="btn search-button btn-md d-none d-md-block ml-4"><i class="fa fa-user-circle"></i>Login</router-link>    
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
